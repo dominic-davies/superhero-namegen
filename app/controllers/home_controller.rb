250d0578651d112719979ebf8f01013eb881e2fd
Dominic Davies <domdaviesit@gmail.com>
2019-02-25 21:09:56 +0000

    index page - added container around card to push content away from navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
+  before_action :common, :only => [:index, :team]
+
+  def common
+    @hero_name = Faker::Superhero.name 
+  end
+
   def index
-    
+
   end
 
   def team
