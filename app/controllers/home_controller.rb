c67be7b4133a1d2be5e352898bdd3252b2d934ca
Dominic Davies <domdaviesit@gmail.com>
2019-02-25 22:33:23 +0000

    added common method to home controller and added faker hero name and power

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
 
   def common
     @hero_name = Faker::Superhero.name 
+    @hero_power = Faker::Superhero.power
+    @avatar = Faker::Avatar.image("my-own-slug", "50x50", "bmp", "set1", "bg1") #=> "https://robohash.org/my-own-slug.bmp?size=50x50&set=set1&bgset=bg1"
+
   end
 
   def index
