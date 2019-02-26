0c952a4d709f76bbf4c82693851e93b2dafd02fb
Dominic Davies <domdaviesit@gmail.com>
2019-02-26 20:41:14 +0000

    added some ruby logic to randomise images using robohash.org

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,29 @@ class HomeController < ApplicationController
   def common
     @hero_name = Faker::Superhero.name 
     @hero_power = Faker::Superhero.power
-    @avatar = Faker::Avatar.image("my-own-slug", "50x50", "bmp", "set1", "bg1") #=> "https://robohash.org/my-own-slug.bmp?size=50x50&set=set1&bgset=bg1"
+    
+    @rand_num = rand(1..1000)
+    
+    @set_number = rand(1..2)
 
+    @bg = rand(1..2)
+
+    # Randomly pic our avatar set
+    if @set_number == 1
+      @set = "set1"
+    else @set_number == 2
+      @set = "set2"
+    end
+
+    # Randomly pic our background
+    if @bg == 1
+      @background = "bg1"
+    else
+      @background = "bg2"
+    end
+
+    # must be at bottom so above calculations run first
+      @avatar = Faker::Avatar.image(@rand_num, "300x300", "png", @set, @background) #=> "https://robohash.org/my-own-slug.bmp?size=50x50&set=set1&bgset=bg1"
   end
 
   def index
